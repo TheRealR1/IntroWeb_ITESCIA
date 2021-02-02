82090fae8610e5bd8b67d966888ce763463468dd
ER1 <erbreiz95@gmail.com>
2021-02-02 15:12:10 +0100

    Fin projet Reacy

--- React/monprojet/src/App.js
+++ React/monprojet/src/App.js
@@ -1,43 +1,90 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const Administration = ({ nom, departement, tel, children }) => {
+const Administration = ({ nom, tel, email }) => {
   return (
-    <div class="panel panel-default">
-      <h5>{nom} - {departement}</h5>
-      <p>Téléphone : {tel}</p>
-      <p>Email : {children}</p>
+    <div className="card col-xs-3" style={{width: "30%"}}>
+      <div className="card-body">
+        <h5 className="card-title">
+          {nom}
+        </h5>
+        <h6 className="card-subtitle mb-2 text-muted">
+          Téléphone : {tel}<br/>
+          Email : {email}
+        </h6>
+      </div>
     </div>
   )
 }
 
 class App extends Component {
-  state = {
-    departements: [
-      "Loire Atlantique",
-      "Maine et Loire",
-      "Mayenne",
-      "Sarthe",
-      "Vendée"
-    ],
-    typeEtablissements: [
-      "Caisse primaire d'assurance maladie",
-      "Chambre de commerce et d'industrie",
-      "Crous et ses antennes"
-    ],
-    resultats: [
-      {
-        nom: "nom"
-      }
-    ]
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      departements: [
+        {
+          nom: "Loire Atlantique",
+          code: 44
+        },
+        {
+          nom: "Maine et Loire",
+          code: 49
+        },
+        {
+          nom: "Mayenne",
+          code: 53
+        },
+        {
+          nom: "Sarthe",
+          code: 72
+        },
+        {
+          nom: "Vendée",
+          code: 85
+        }
+      ],
+      typeEtablissements: [
+        {
+          nom: "Caisse primaire d'assurance maladie",
+          type: "cpam"
+        },
+        {
+          nom: "Chambre de commerce et d'industrie",
+          type: "cci"
+        },
+        {
+          nom: "Crous et ses antennes",
+          type: "crous"
+        }
+      ],
+      resultats: [
+        {
+          features: []
+        }
+      ]
+    }
+
+    this.rechercherAdministration.bind(this);
+    this.viderRecherche.bind(this);
+
+  }
+  
+
+  rechercherAdministration = () => {
+    var departCode = document.getElementById("departCode").value;
+    var typeEtab = document.getElementById("typeEtab").value;
+
+    fetch('https://etablissements-publics.api.gouv.fr/v3/departements/' + departCode + '/' + typeEtab)
+    .then(res => res.json())
+    .then((data) => {
+      this.setState({ resultats: data });
+    })
+    .catch(console.log)
   }
 
-  rechercherAdministration() {
-    
-  }
-
-  viderRecherche() {
-    
+  viderRecherche = () => {
+    this.setState({ resultats: []});
   }
 
   render() {
@@ -45,30 +92,34 @@ class App extends Component {
       <div className="App">
         
         <h1>Trouver son administration en Pays de la Loire</h1>
-        <select>
+        <select id="departCode">
           {
             this.state.departements.map(
               (depart,index) =>
-                <option value={index}>{depart}</option>
+                <option key={index} value={depart.code}>{depart.nom}</option>
               )
           }
         </select>
-        <select>
+        <select id="typeEtab">
           {
             this.state.typeEtablissements.map(
               (typeEtab,index) =>
-                <option value={index}>{typeEtab}</option>
+                <option key={index} value={typeEtab.type}>{typeEtab.nom}</option>
               )
           }
         </select>
         <button onClick={ this.rechercherAdministration }>Rechercher une administration</button>
         <button onClick={ this.viderRecherche }>Vider la recherche</button>
-        <br/>
-        <Administration nom="Centre local des oeuvres" departement="Maine-et-Loire" tel="+33 2 41 25 45 80">
-          crous.clous@nantes.fr
-        </Administration>
-        <Administration nom="Centre local des oeuvres" departement="Maine-et-Loire" tel="+33 2 41 25 45 80"/>
 
+        <div className="row offset-1" style={{ marginTop: "20px"}}>
+          {
+            this.state.resultats["features"] &&
+            this.state.resultats["features"].map(
+              (res,index) =>
+                <Administration key={index} nom={res.properties.nom} tel={res.properties.telephone} email={res.properties.email} />
+              )
+          }
+        </div>
       </div>
     );
   }
